0b84c0fb1c1d506af4dc9338c15d6320d97fa768
itzvicky435 <itzvicky435@gmail.com>
2024-10-26 12:04:36 +0000

    commit

--- javascriptCode.js
+++ javascriptCode.js
@@ -2,30 +2,30 @@
 
 // 1. Second Higesht Number 
 
-arr=[5,9,6,8,4,3,9,6,5,7,1,3];
+// arr=[5,9,6,8,4,3,9,6,5,7,1,3];
 
-const uniqueArr = [...new Set(arr)];
+// const uniqueArr = [...new Set(arr)];
 
-const sortedUniqArr = uniqueArr.sort((a,b) => b -a);
+// const sortedUniqArr = uniqueArr.sort((a,b) => b -a);
 
-const secondHighest = sortedUniqArr[1];
+// const secondHighest = sortedUniqArr[1];
 
-console.log("second highest number :-", secondHighest)
+// console.log("second highest number :-", secondHighest)
 
 // arr = [1,2,2,3,2,4,5,4]
 
 // 2. example
-var arr = [5, 9, 6, 8, 4, 3, 9, 6, 5, 7, 1, 3];
-var secondHighests = [...new Set(arr)].sort((a, b) => b - a)[1];
+// var arr = [5, 9, 6, 8, 4, 3, 9, 6, 5, 7, 1, 3];
+// var secondHighests = [...new Set(arr)].sort((a, b) => b - a)[1];
 
-console.log("Second highest number:", secondHighests);
+// console.log("Second highest number:", secondHighests);
 
 
 // 3 Higest Number in Array 
 
-let numbers = [5,3,6,8,10];
-let highestNumber = Math.max(...numbers);
-console.log("higest number"+ highestNumber)
+// let numbers = [5,3,6,8,10];
+// let highestNumber = Math.max(...numbers);
+// console.log("higest number"+ highestNumber)
 
 
 
@@ -66,6 +66,120 @@ console.log("higest number"+ highestNumber)
 // console.log(uniqueFruits)
 
 
+// const arr = [1, 2, 2, 3, 4, 4, 5];
+// const duplicates = [];
+
+// // Using a Set to efficiently find duplicates
+// const uniqueElements = new Set();
+// for (const element of arr) {
+//   if (uniqueElements.has(element)) {
+//     duplicates.push(element);
+//   } else {
+//     uniqueElements.add(element);
+//   }
+// }
+// console.log("Duplicate elements:", duplicates); // Output: [2, 4]
+
+//  Find a duplicate element in an array
+
+// const myarray =  [1, 2, 3, 3, 4, 6, 7, 7 ,7 , 6 , 1, 8, 9, 10]
+// const duplicate = [...new Set(myarray.filter((item, index) => myarray.indexOf(item) !== index))]
+
+// console.log("duplicate elements :-", duplicate)
+
+//  Find a how many Duplicate values in an array
+
+// const myarray1 =  [1, 2, 3, 3, 4, 6, 7, 7 ,7 , 6 , 1, 8, 9, 10]
+
+// const countDuplicates =  myarray1.length - new Set(myarray1).size;
+// const uniqueElements1 = arr.length - [...new Set(myarray1)].length;
+// console.log("unique elements :-", uniqueElements1)
+
+// console.log("Duplicate elementssss :-", countDuplicates)
+
+
+// const arr = [1, 2, 2, 3, 4, 4, 5, 7 ,6,7,0, 5, 6, 7, 8, 9, 10,2,8,9,10];
+
+// const duplicateValues = [...new Set(arr.filter((item, index) => arr.indexOf(item) !== index))];
+// const numberOfDuplicateValues = duplicateValues.length;
+
+// console.log("Number of duplicate values:", numberOfDuplicateValues); 
+
+
+// let age = [12, 15, 18, 20, 19,4,5,1,2]
+// age.sort((a,b) => a - b);
+// age.sort()
+// console.log(age)
+
+// // how to sequenct type of array
+
+// const arr = [1, 2, 2, 3, 4, 4, 5, 7 ,6,7,0, 5, 6, 7, 8, 9, 10,2,8,9,10];
+
+// const uniqueElements = arr.filter((item, index) => arr.indexOf(item) === index);
+// console.log(uniqueElements)
+
+// 1 How do you merge two arrays in javascript ?
+
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+// const mergedArray = array1.concat(array2);
+
+// const mergedArray1 = [...array1, ...array2];
+// console.log(mergedArray)
+
+
+// 2  How to remove duplicates from an array in javascript 
+
+// const array3  = [1, 2, 2, 3, 4, 4, 5, 7 ,6,7,0, 5, 6, 7, 8, 9, 10,2,8,9,1];
+// const newarray = [...new Set(array3)];
+
+// console.log(newarray)
+
+
+// 3  How to count duplicate values in an array
+
+// const  array4 = [1, 2, 2, 3, 4, 4, 5, 7 ,6,7,0, 5, 6, 7, 8, 9, 10,2,8,9,1];
+// const countDuplicates = array4.length - new Set(array4).size;
+// console.log(countDuplicates)
+
+// 4  How to remove specific element from an array 
+
+// const array5 = [1,2, 3,4,5,6,7,8,9];
+// const removeElement = array5.filter(item => item !== 7);
+// console.log(removeElement);
+
+
+//  5 How to Find all unique values in an array of objects by a specific key ?
+
+// const array6 = [
+//     {id: 1,name : 'john'},
+//     {id: 2, name : 'smith'},
+//     {id: 3, name: 'show'},
+//     {id: 4, name : 'smith'},
+//     {id: 5, name: 'smith'}
+// ];
+// const uniqueNames = [...new Set(array6.map(item => item.name))];
+// console.log(uniqueNames)
+
+//  6 How to all unique values in an array of objects by a specfic key and all unique duplicate values remove 
+
+const array7 = [
+    {id: 1,name : 'john'},
+    {id: 2, name : 'smith'},
+    {id: 3, name: 'show'},
+    {id: 4, name : 'smith'},
+    {id: 5, name: 'smith'}
+];
+const uniqueNames = [...new Set(array7.map(item => item.name))];
+console.log(uniqueNames)
+
+//  7 how to print trangle star pattern in javascript ?
+
+//   How can you flatten a nested array into a single flat array?
+
+const arr = [1, [2, 3], [4, [5, 6]]];
+const flatArray = arr.flat(Infinity);
+console.log(flatArray)
 
 
 
